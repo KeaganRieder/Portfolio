a49fdd90293970b88daf95607eb7435f8375b9f1
KeaganRieder <keagan@rieder.ca>
2024-11-09 14:28:27 -0700

    site restructuring complete

---
+++ scripts/NavScripts.js
@@ -0,0 +1,40 @@
+/* 
+-----------------------------------------------------------
+file contains definitions for scripts used in 
+the navigation of site contents
+-----------------------------------------------------------
+*/
+
+// gives functionality to side bar/files to make them open the different 
+// sections in the portfolio
+function OpenFile(sectionId){
+    var section = document.getElementById(sectionId);
+    if (section.style.display == "none" || section.style.display == "") {
+        section.style.display = "grid";
+    }
+}
+
+// gives functionality to files x button to close the file
+function CloseFile(sectionId){
+    var section = document.getElementById(sectionId);
+    section.style.display = "none";
+
+}
+
+// gives functionality to move between different sections in portfolio
+function ToggleSection(sectionId, sectionTabId){
+    var section = document.getElementById(sectionId);
+    var sectionTab =  document.getElementById(sectionTabId);
+
+    if (section.style.display == "none" || section.style.display == "") {
+        section.style.display = "grid";
+    } else {
+        section.style.display = "none";
+    }
+
+    if (sectionTab.style.display == "none" || sectionTab.style.display == "") {
+        sectionTab.style.display = "inline-block";
+    } else {
+        sectionTab.style.display = "none";
+    }
+}
